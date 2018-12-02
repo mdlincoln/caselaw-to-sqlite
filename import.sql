006d686b8ee879eac4e2d0a708d1bde201d99bef
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-02 18:09:56 -0500

    use more informative id names

--- import.sql
+++ import.sql
@@ -1,21 +1,21 @@
 PRAGMA foreign_keys = ON;
 BEGIN;
 CREATE TABLE IF NOT EXISTS courts (
-  id INTEGER PRIMARY KEY NOT NULL,
+  court_id INTEGER PRIMARY KEY NOT NULL,
   `name` TEXT NOT NULL,
   name_abbreviation TEXT NOT NULL,
   jurisdiction_url TEXT,
   slug TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS jurisdictions (
-  id INTEGER PRIMARY KEY NOT NULL,
+  jurisdiction_id INTEGER PRIMARY KEY NOT NULL,
   slug TEXT NOT NULL,
   `name` TEXT NOT NULL,
   name_long TEXT NOT NULL,
   whitelisted TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS cases (
-  id INTEGER PRIMARY KEY NOT NULL,
+  case_id INTEGER PRIMARY KEY NOT NULL,
   `name` TEXT NOT NULL,
   name_abbreviation TEXT NOT NULL,
   decision_date TEXT NOT NULL,
@@ -28,30 +28,30 @@ CREATE TABLE IF NOT EXISTS cases (
   jurisdiction_id INTEGER NOT NULL,
   `status` TEXT NOT NULL,
   head_matter TEXT NOT NULL,
-  FOREIGN KEY(court_id) REFERENCES courts(id),
-  FOREIGN KEY(jurisdiction_id) REFERENCES jurisdictions(id)
+  FOREIGN KEY(court_id) REFERENCES courts(court_id),
+  FOREIGN KEY(jurisdiction_id) REFERENCES jurisdictions(jurisdiction_id)
 );
 CREATE TABLE IF NOT EXISTS opinions (
-  id INTEGER NOT NULL,
+  case_id INTEGER NOT NULL,
   `type` TEXT NOT NULL,
   `text` TEXT NOT NULL,
   author TEXT NOT NULL,
-  FOREIGN KEY (id) REFERENCES cases(id)
+  FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
 CREATE TABLE IF NOT EXISTS parties (
-  id INTEGER NOT NULL,
+  case_id INTEGER NOT NULL,
   party TEXT NOT NULL,
-  FOREIGN KEY (id) REFERENCES cases(id)
+  FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
 CREATE TABLE IF NOT EXISTS attorneys (
-  id INTEGER NOT NULL,
+  case_id INTEGER NOT NULL,
   attorney TEXT NOT NULL,
-  FOREIGN KEY (id) REFERENCES cases(id)
+  FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
 CREATE TABLE IF NOT EXISTS judges (
-  id INTEGER NOT NULL,
+  case_id INTEGER NOT NULL,
   judge TEXT NOT NULL,
-  FOREIGN KEY (id) REFERENCES cases(id)
+  FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
 .mode csv
 .import courts.csv courts
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS judges (
 .import attorneys.csv attorneys
 CREATE INDEX IF NOT EXISTS case_court_id ON cases(court_id);
 CREATE INDEX IF NOT EXISTS case_jurisdiction_id ON cases(jurisdiction_id);
-CREATE INDEX IF NOT EXISTS opinion_case_id ON opinions(id);
-CREATE INDEX IF NOT EXISTS parties_case_id ON parties(id);
-CREATE INDEX IF NOT EXISTS attorneys_case_id ON attorneys(id);
-CREATE INDEX IF NOT EXISTS judges_case_id ON judges(id);
+CREATE INDEX IF NOT EXISTS opinion_case_id ON opinions(case_id);
+CREATE INDEX IF NOT EXISTS parties_case_id ON parties(case_id);
+CREATE INDEX IF NOT EXISTS attorneys_case_id ON attorneys(case_id);
+CREATE INDEX IF NOT EXISTS judges_case_id ON judges(case_id);
 COMMIT;
