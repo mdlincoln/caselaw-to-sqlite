4c95fbab94c48d184ec649582ffb18c501591575
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-03 09:48:24 -0500

    preface colnames so they are unique and intelligible across tables

--- import.sql
+++ import.sql
@@ -2,40 +2,40 @@ PRAGMA foreign_keys = ON;
 BEGIN;
 CREATE TABLE IF NOT EXISTS courts (
   court_id INTEGER PRIMARY KEY NOT NULL,
-  `name` TEXT NOT NULL,
-  name_abbreviation TEXT NOT NULL,
-  jurisdiction_url TEXT,
-  slug TEXT NOT NULL
+  court_name TEXT NOT NULL,
+  court_name_abbreviation TEXT NOT NULL,
+  court_jurisdiction_url TEXT,
+  court_slug TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS jurisdictions (
   jurisdiction_id INTEGER PRIMARY KEY NOT NULL,
-  slug TEXT NOT NULL,
-  `name` TEXT NOT NULL,
-  name_long TEXT NOT NULL,
-  whitelisted TEXT NOT NULL
+  jurisdiction_slug TEXT NOT NULL,
+  jurisdiction_name TEXT NOT NULL,
+  jurisdiction_name_long TEXT NOT NULL,
+  jurisdiction_whitelisted TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS cases (
   case_id INTEGER PRIMARY KEY NOT NULL,
-  `name` TEXT NOT NULL,
-  name_abbreviation TEXT NOT NULL,
-  decision_date TEXT NOT NULL,
-  docket_number TEXT NOT NULL,
-  first_page INTEGER NOT NULL,
-  last_page INTEGER NOT NULL,
-  volume_number INTEGER NOT NULL,
-  reporter TEXT NOT NULL,
+  case_name TEXT NOT NULL,
+  case_name_abbreviation TEXT NOT NULL,
+  case_decision_date TEXT NOT NULL,
+  case_docket_number TEXT NOT NULL,
+  case_first_page INTEGER NOT NULL,
+  case_last_page INTEGER NOT NULL,
+  case_volume_number INTEGER NOT NULL,
+  case_reporter TEXT NOT NULL,
   court_id INTEGER NOT NULL,
   jurisdiction_id INTEGER NOT NULL,
-  `status` TEXT NOT NULL,
-  head_matter TEXT NOT NULL,
+  case_status TEXT NOT NULL,
+  case_head_matter TEXT NOT NULL,
   FOREIGN KEY(court_id) REFERENCES courts(court_id),
   FOREIGN KEY(jurisdiction_id) REFERENCES jurisdictions(jurisdiction_id)
 );
 CREATE TABLE IF NOT EXISTS opinions (
   case_id INTEGER NOT NULL,
-  `type` TEXT NOT NULL,
-  `text` TEXT NOT NULL,
-  author TEXT NOT NULL,
+  opinion_type TEXT NOT NULL,
+  opinion_text TEXT NOT NULL,
+  opinion_author TEXT NOT NULL,
   FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
 CREATE TABLE IF NOT EXISTS parties (
